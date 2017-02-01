4c7cae8d44f3cf6a1f6da89c91e06e6c52faf827
Matthew-Burfield <m1.burfield@gmail.com>
2017-02-01 13:37:34 +1000

    First commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const gameData = [
+  [{active:false},{active:false},{active:false},{active:false}],
+  [{active:false},{active:false},{active:false},{active:false}],
+  [{active:false},{active:false},{active:false},{active:false}],
+  [{active:false},{active:false},{active:false},{active:false}],
+];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      gameData,
+    };
+    this.activateGameSquare = this.activateGameSquare.bind(this);
+  } 
+  activateGameSquare(row,col) {
+    const newGameState = [...gameData];
+    newGameState[row][col] = {active:true};
+    this.setState({
+      gameData: newGameState,
+    })
+  }
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>Welcome to React</h2>
+        </div>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+        <div className="gameBoard">
+          {this.state.gameData.map((row, index) => <GameBoardRow key={index} gameRow={row} row={index} activateGameSquare={this.activateGameSquare} />)}
+        </div>
+      </div>
+    );
+  }
+}
+
+const GameBoardRow = ({gameRow, row, activateGameSquare}) =>
+  <div className="game-board-row">
+    {gameRow.map((square, index) => <GameSquare key={index} square={square} row={row} col={index} activateGameSquare={activateGameSquare} />)}
+  </div>
+
+const GameSquare = ({square, row, col, activateGameSquare}) => 
+  <div className={`square ${square.active}`} onClick={() => activateGameSquare(row, col)}></div>
+
+export default App;
